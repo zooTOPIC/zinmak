79607223fb2a72e83f3d7a02d2a154af3ac2e054
KYSgh <162289300+KYSgh@users.noreply.github.com>
2024-03-21 13:42:38 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,17 @@
-!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/practice-githubAction-deploy-0.0.1-SNAPSHOT.jar)
-JAR_NAME=$(basename $BUILD_JAR)
+#!/bin/bash
 
-echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
-
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
-
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
-cp $BUILD_JAR $DEPLOY_PATH
-
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(sudo docker container ls -q)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
+  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
-  sudo kill -9 $CURRENT_PID
+  echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
+  sudo docker stop $CURRENT_PID
   sleep 5
 fi
 
-
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+cd /home/ec2-user/app
+sudo docker build -t tcat-api-spring-boot-docker .
+sudo docker run -d -p 8080:8080 tcat-api-spring-boot-docker
